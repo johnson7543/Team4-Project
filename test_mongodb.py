99bd3605cff4a1b9e3b20556dbccd924cf2787d7
YuKai <johnson7543@cycu.org.tw>
2020-07-05 19:59:33 +0800

    Update test_mongodb.py

--- test_mongodb.py
+++ test_mongodb.py
@@ -6,7 +6,7 @@ import pymongo
 def runMongo(data):    
     
     mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://heroku_95jb1hvd:5mvk8kou4g9kfpl3uo2knehlmg@ds339968.mlab.com:39968/heroku_95jb1hvd')  
-    client = pymongo.Connection(mongo_url)
+    client = pymongo.MongoClient(mongo_url)
     db = client.heroku_95jb1hvd
     collection = db.Test
     # mydict = { "name": "YuKai Wang", "Email": "johnson7543@cycu.org.tw", "brith": "1998/09/21" }
