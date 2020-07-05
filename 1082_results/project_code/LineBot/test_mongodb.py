10bf07d1dd4d5654dedd54b0c7cbae8cacc32816
YuKai <johnson7543@cycu.org.tw>
2020-07-05 17:40:00 +0800

    fuck

--- 1082_results/project_code/LineBot/test_mongodb.py
+++ 1082_results/project_code/LineBot/test_mongodb.py
@@ -1,18 +1,16 @@
 # import
-from pymongo import MongoClient
-# from bson.objectid import ObjectId #這東西再透過ObjectID去尋找的時候會用到
+import os
+import pymongo
 
 # connection
-def runMongo(data):
-    client = MongoClient("mongodb+srv://johnson7543:BfT5BEThq3deNBxJ@cluster0-84ii5.mongodb.net/Test?retryWrites=true&w=majority")
-    db = client.Test
-    collection = db.testLineBot
+def runMongo(data):    
     
-    # test if connection success
-    collection.stats  # 如果沒有error，你就連線成功了。
-    print (data)
+    mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://heroku_95jb1hvd:5mvk8kou4g9kfpl3uo2knehlmg@ds339968.mlab.com:39968/heroku_95jb1hvd')  
+    client = pymongo.Connection(mongo_url)
+    db = client.heroku_95jb1hvd
+    collection = db.Test
     # mydict = { "name": "YuKai Wang", "Email": "johnson7543@cycu.org.tw", "brith": "1998/09/21" }
     
     if (data) :
         mydict ={"action": data["result"]["parameters"]["action"]}
-        collection.insert_one(mydict) 
+        collection.insert(mydict) 
