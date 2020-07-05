b168bacff85ef0e34a1c13d6d8bdd69437048e31
YuKai <johnson7543@cycu.org.tw>
2020-07-05 20:23:35 +0800

    OK

--- test_mongodb.py
+++ test_mongodb.py
@@ -6,6 +6,8 @@ import pymongo
 def runMongo(data):    
     
     mongo_url = os.getenv('MONGOLAB_URI', 'mongodb://heroku_95jb1hvd:5mvk8kou4g9kfpl3uo2knehlmg@ds339968.mlab.com:39968/heroku_95jb1hvd?retryWrites=false')  
+    # must add "retryWrites=false" at the end of the url for no reason
+    
     client = pymongo.MongoClient(mongo_url)
     db = client.heroku_95jb1hvd
     collection = db.Test
