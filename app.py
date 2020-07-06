272b97223d928c3c1a3fcfa440db6687f46661da
YuKai <johnson7543@cycu.org.tw>
2020-07-06 23:36:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,10 @@ def handle_message(event):#此函數接收LINE傳過來的資訊並貼上"event"
     #                    "text":"使用者傳來的文字信息內容"}}
         
     #選擇參數"text"將他丟給Dialogflow去解析其內容
-    responseJson = parse_user_text(event.message.text)
-    # userID = event.user_id
+    responseJson = []
+    responseJson.append(parse_user_text(event.message.text))
+    responseJson.append(event.user_id)
+    
     data = test_mongodb.runMongo(responseJson, event.message.text) # 嘗試把dialogflow回傳的存入mongodb
     # 以及從db拿取獎學金資訊、研究所資訊...etc(暫時)
     data_str = "".join(str(i.get('action'))+'\n' for i in list(data))
@@ -84,7 +86,7 @@ def handle_message(event):#此函數接收LINE傳過來的資訊並貼上"event"
     #把message的"text"這個項目改成此訊息經由dialogflow解析後的action
     
     #回傳訊息的製作，更改messgae裡面text的內容
-    message = TextSendMessage(text = '你的Action : ' + responseJson["result"]["parameters"]["action"] + '\n'
+    message = TextSendMessage(text = '你的Action : ' + responseJson[0].["result"]["parameters"]["action"] + '\n'
                               + '以下是我幫你找到的資料 ：\n' + data_str )
     
     
