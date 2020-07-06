bb8919598737d95fb592410aa6096faa707d3662
YuKai <johnson7543@cycu.org.tw>
2020-07-06 17:30:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def handle_message(event):#此函數接收LINE傳過來的資訊並貼上"event"
     # 以及從db拿取獎學金資訊、研究所資訊...etc(暫時)
     data_str = "".join(str(list(data)))
     # 型別轉換 就是要打破strongly typed
+    # Cursor -> list(dict) -> string
     
     #TextSendMessage是要執行的動作，LINE還提供了其他包括：ImageSendMessage、VideoSendMessage、StickerSendMessage等等的許多許多動作
     #message也是一個json物件(或許跟event長很像)
