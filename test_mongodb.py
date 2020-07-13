d7d7f1949d3bcba8699d5283189d3198d9d855e7
YuKai <johnson7543@cycu.org.tw>
2020-07-13 20:50:52 +0800

    abc

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,15 +1,15 @@
 # import
-import os
+# import os
 import pymongo
 import select_mongodb
 
 # connection
 def runMongo(response, text):    
     
-    mongo_url = os.getenv('MONGO_CONNECTION', 'mongodb+srv://johnson7543:BfT5BEThq3deNBxJ@cluster0-84ii5.mongodb.net/Test?retryWrites=true&w=majority')  
+    # mongo_url = os.getenv('MONGO_CONNECTION', 'mongodb+srv://johnson7543:BfT5BEThq3deNBxJ@cluster0-84ii5.mongodb.net/Test?retryWrites=true&w=majority')  
     # must add "retryWrites=false" at the end of the url for no reason
     
-    client = pymongo.MongoClient(mongo_url)
+    client = pymongo.MongoClient("mongodb+srv://johnson7543:BfT5BEThq3deNBxJ@cluster0-84ii5.mongodb.net/Test?retryWrites=true&w=majority")
     db = client.Test
     collection = db.testLineBot
     # mydict = { "name": "YuKai Wang", "Email": "johnson7543@cycu.org.tw", "brith": "1998/09/21" }
