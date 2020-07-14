ff9332982d1173703e7395a06a3888bc601c9b0d
YuKai <johnson7543@cycu.org.tw>
2020-07-14 19:44:37 +0800

    Update select_mongodb.py

--- select_mongodb.py
+++ select_mongodb.py
@@ -4,10 +4,10 @@ def arrange():
     return
 
 
-def seldata(userid, text):
+def seldata(collection, text):
 
     if '獎學金' in text:
-        collection = db[str(userid)] # just testing if it wil work
+        collection = collection # just testing if it wil work
         # 'db.Scholarship_Info' is the correct db collection to select from
     
     return collection.find() # no parameter means all data in the collection
