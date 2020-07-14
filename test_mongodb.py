f15772e439ccd38b833e54b850e9c87c7c1ec2da
YuKai <johnson7543@cycu.org.tw>
2020-07-14 19:13:27 +0800

    id

--- test_mongodb.py
+++ test_mongodb.py
@@ -12,9 +12,10 @@ def runMongo(response, text):
     # Since the mlab-MongoDB will be removed from heroku app on November 10, we are now using MongoDB Atlas directly
     # Btw, there is a new app on heroku which support accessing  mongodb with no free plan, fuck you.
     
+    userid = response[1]
     client = pymongo.MongoClient("mongodb+srv://johnson7543:BfT5BEThq3deNBxJ@cluster0-84ii5.mongodb.net/Test?retryWrites=true&w=majority")
     db = client.Test
-    collection = db.testLineBot
+    collection = db[str(userid)]
     # mydict = { "name": "YuKai Wang", "Email": "johnson7543@cycu.org.tw", "brith": "1998/09/21" }
     
     if (response) :
