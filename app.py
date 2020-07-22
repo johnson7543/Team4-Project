bdb8737f571cdec62e770bccb1ca4942b26689a5
YuKai <johnson7543@cycu.org.tw>
2020-07-22 21:07:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def handle_message(event):#此函數接收LINE傳過來的資訊並貼上"event"
     # 以及從db拿取獎學金資訊、研究所資訊...etc(暫時)
     # 然而db拿出來的資料有我們不要的東西 e.g. Obj id...
         data_str = "".join(str(i.get('target')[0])+'\n' for i in list(data_db))
-        print(data_str)
-        print(fulfi_text)
+        print(type(data_str))
+        print(type(fulfi_text))
     # 型別轉換 就是要打破strongly typed
     # Cursor -> list(dict) -> string
     #TextSendMessage是要執行的動作，LINE還提供了其他包括：ImageSendMessage、VideoSendMessage、StickerSendMessage等等的許多許多動作
