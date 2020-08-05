5fc5eb591b0f8d8704512c3f8d34f2ef64b99dda
YuKai <johnson7543@cycu.org.tw>
2020-08-05 17:55:53 +0800

    Update select_mongodb.py

--- select_mongodb.py
+++ select_mongodb.py
@@ -11,6 +11,8 @@ def seldata(sel_client, response):
         db = sel_client.blog
         collection = db.posts
     
-    return collection.find() # no parameter means all data in the collection
+        return collection.find({ "Apply" : "清寒"}); # no parameter means all data in the collection
+    else:
+        return
     
     
