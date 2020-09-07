e11cf8c0b1b7ab937974f3dd95f2dc13ccedfb1e
YuKai <johnson7543@cycu.org.tw>
2020-09-08 00:30:18 +0800

    aaa

--- select_mongodb.py
+++ select_mongodb.py
@@ -1,3 +1,4 @@
+import re
 def arrange_scholarship(sel_client, data):
     select_db = "Total_Scholarship"
     db = sel_client[select_db]
@@ -15,12 +16,18 @@ def arrange_scholarship(sel_client, data):
     if ( data["result"]["contexts"][0]["parameters"]["number"]):
         score_num = int(data["result"]["contexts"][0]["parameters"]["number"])
     
+    print(temp_category,temp_score,score_num)
+    
     if '清寒類' in temp_category:
         select_col = "清寒類"
         collection = db[select_col]
         
         if '學業成績' in temp_score and 0 <= score_num <= 100 :
-            data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
+            if '大學部' or '研究所' in temp_category :
+              print( temp_category )
+              data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, "申請身分" : { '$not' : {'$regex': '大一'} } } ] } )
+            else :
+              data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
         else :
           data_db = collection.find() # no score filter request, get all data     
         data_str = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
@@ -30,28 +37,45 @@ def arrange_scholarship(sel_client, data):
         collection = db[select_col]
         
         if '學業成績' in temp_score and 0 <= score_num <= 100 :
-          data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
+          if '大學部' or '研究所' in temp_category :
+            data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, "申請身分" : { '$not' : {'$regex': '大一'} } } ] } )
+          else :
+            data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
         else :
           data_db = collection.find() # no score filter request, get all data
         data_str_all = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
-        
         return data_str + data_str_all
         
     elif '學業成績' in temp_score and 0 <= score_num <= 100 :
+        print("123")
         select_col = "所有類" # all data
         collection = db[select_col]
-        data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
-        data_str_all = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
         
-        return data_str + text
+        if '學業成績' in temp_score and 0 <= score_num <= 100 :
+          if '大學部' or '研究所' in temp_category :
+            print( temp_category )
+            data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, 
+                                                    '申請身分' : { '$not' : {'$regex': '大一'} },
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] }} ] } )
+          else :
+            data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, 
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
+                    
+        data_str_all = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
+        return data_str_all
     
     else:
         select_col = "所有類" # all data collection
         collection = db[select_col]
-        data_db = collection.find() # no parameter means all data in the collection
-        data_str = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
-        
-        return data_str + text
+        if '學業成績' in temp_score and 0 <= score_num <= 100 :
+          if '大學部' or '研究所' in temp_category :
+            print( temp_category )
+            data_db = collection.find( {'$and': [ { '申請身分' : { '$not' : {'$regex': '大一'} },
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] }} ] } )
+          else :
+            data_db = collection.find( {'$and': [ { '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
+        data_str_all = "".join(str(i.get('名稱'))+'\n' for i in list(data_db))
+        return data_str_all
 
 
 
