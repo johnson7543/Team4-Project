742b564aabff12e92b15921f333ae7b78072b106
YuKai <johnson7543@cycu.org.tw>
2020-09-08 16:33:39 +0800

    Update select_mongodb.py

--- select_mongodb.py
+++ select_mongodb.py
@@ -15,14 +15,12 @@ def arrange_scholarship(sel_client, data):
     if ( data["result"]["contexts"][0]["parameters"]["number"]):
         score_num = int(data["result"]["contexts"][0]["parameters"]["number"])
     
-    
     if '清寒類' in temp_category:
         select_col = "清寒類"
         collection = db[select_col]
         
         if '學業成績' in temp_score and 0 <= score_num <= 100 :
             if '大學部' or '研究所' in temp_category :
-              print( temp_category )
               data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, "申請身分" : { '$not' : {'$regex': '大一'} } } ] } )
             else :
               data_db = collection.find( { '學業成績' : { '$lte' : score_num } } )
@@ -45,33 +43,28 @@ def arrange_scholarship(sel_client, data):
         return data_str + data_str_all
         
     elif '學業成績' in temp_score and 0 <= score_num <= 100 :
-        print("123")
         select_col = "所有類" # all data
         collection = db[select_col]
         
         if '學業成績' in temp_score and 0 <= score_num <= 100 :
           if '大學部' or '研究所' in temp_category :
-            print( temp_category )
             data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, 
                                                     '申請身分' : { '$not' : {'$regex': '大一'} },
-                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] }} ] } )
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
           else :
             data_db = collection.find( {'$and': [ { '學業成績' : {'$lte' : score_num}, 
-                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
-                    
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )                    
         data_str_all = "".join(str(i.get('名稱'))+'\n'+ str(i.get('網址'))+'\n\n' for i in list(data_db))
         return data_str_all
     
     else:
         select_col = "所有類" # all data collection
         collection = db[select_col]
-        if '學業成績' in temp_score and 0 <= score_num <= 100 :
-          if '大學部' or '研究所' in temp_category :
-            print( temp_category )
-            data_db = collection.find( {'$and': [ { '申請身分' : { '$not' : {'$regex': '大一'} },
-                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] }} ] } )
-          else :
-            data_db = collection.find( {'$and': [ { '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
+        if '大學部' or '研究所' in temp_category :
+          data_db = collection.find( {'$and': [ { '申請身分' : { '$not' : {'$regex': '大一'} },
+                                                    '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
+        else :
+          data_db = collection.find( {'$and': [ { '申請資格' : {'$nin': [ re.compile(u'清寒'),re.compile(u'低收'),re.compile(u'弱勢'),re.compile(u'急難') ] } } ] } )
         data_str_all = "".join(str(i.get('名稱'))+'\n'+ str(i.get('網址'))+'\n\n' for i in list(data_db))
         return data_str_all
 
