1b9eee8753f4f7034006f13ee80f977439e8186f
YuKai <johnson7543@cycu.org.tw>
2020-10-14 13:52:20 +0800

    Update test_mongodb.py

--- test_mongodb.py
+++ test_mongodb.py
@@ -30,9 +30,8 @@ def runMongo(response, data):
         print()
         
         profile_photo = str(profile.picture_url)
-        profile_name = str(profile['display_name'])
-
-        
+        profile_name = str(profile.display_name)
+      
         if ( data["result"]["contexts"][0]["parameters"]["ApplicationCategory.original"] ) :
           info = str(data["result"]["contexts"][0]["parameters"]["ApplicationCategory.original"])
         else :
