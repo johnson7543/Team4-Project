8261fb3fe648b79043f2975b235b452c59052f9f
YuKai <johnson7543@cycu.org.tw>
2020-10-14 12:29:08 +0800

    Update get_user_profile.py

--- get_user_profile.py
+++ get_user_profile.py
@@ -1,7 +1,7 @@
 from linebot import LineBotApi
 from linebot.exceptions import LineBotApiError
 
-def getProfile( userid = ''  ) :
+def getProfile( userid ) :
   line_bot_api = LineBotApi('l8HIzKnuKYtgSCLb5VG2VcBPoaEM3xWnDZQcGwoGkBWnpV8aji5gPeKDP1kTy/CxmskDdaND9kuV05D1GDEcuUWkwnSmv2QewSuSdU/4lZtZI188/NS9YA5vVEEjI0Zo1YBa9y/pc77fUcZlQTx7EAdB04t89/1O/w1cDnyilFU=')
 
   try:
