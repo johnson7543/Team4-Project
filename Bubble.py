0bc0fb34c479e5a7bca464c489453af2a1ce4c99
LIN <YJLin19990410@gmail.com>
2020-10-20 22:09:11 +0800

    Update Bubble.py

--- Bubble.py
+++ Bubble.py
@@ -19,7 +19,7 @@ Bubble_Type =     {
             "weight": "bold",
             "position": "relative",
             "align": "center",
-            "wrap":"true",
+            "wrap": true,
             "action": {
               "type": "uri",
               "label": "action",
