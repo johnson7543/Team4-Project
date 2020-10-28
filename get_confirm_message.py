06cf44d86eb87eca3013d7773d4af4755695574b
YuKai <johnson7543@cycu.org.tw>
2020-10-28 17:53:03 +0800

    Update get_confirm_message.py

--- get_confirm_message.py
+++ get_confirm_message.py
@@ -18,9 +18,9 @@ def get_message(data) :
   temp_all = temp_category + temp_score
 
   if ( temp_all ) :   
-    text = "請問是想要篩選以下條件嗎？" + "\n"
+    text = "這是你所想要篩選的條件嗎？" + "\n"
     return text + temp_category + temp_score
   else :
-    text = "您沒有篩選任何條件哦？ 確定嗎？"
+    text = "你沒有篩選任何條件哦？ 確定嗎？"
     return text
         
